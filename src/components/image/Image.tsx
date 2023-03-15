49c8ac9558d32408cdc64410a2c0d6fac92aecde
mcjothykrishnan <mcjothykrishnan@gmail.com>
2023-03-15 12:13:26 +0530

    NextImagepdate

--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -1,6 +1,6 @@
 import { Box, ChakraComponent } from '@chakra-ui/react'
 import * as React from 'react'
-import NextImage from 'next/image'
+import NextImage from "next/legacy/image"
 import { ComponentProps } from 'react'
 
 interface ImageProps extends ComponentProps<ChakraComponent<'div', {}>> {}
